93316c927274020c97ff1e9c3cd83dd708e3abd3
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2023-11-04 09:32:08 -0600

    Adding New .cs and Writing program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "School Bus Driver";
+        job1._company = "SR School District";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "CS Supervisor";
+        job2._company = "SkyWest";
+        job2._startYear = 2013;
+        job2._endYear = 2021;
+
+        Resume myResume = new Resume();
+        myResume._name = "Julie Crowther";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
