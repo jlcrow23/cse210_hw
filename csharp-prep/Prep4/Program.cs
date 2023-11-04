2fba380723230eadebc7e66db3157da6542a4909
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2023-11-04 08:21:03 -0600

    Prep 4 completion

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // //List words
+        // List<string> words = newList<string>();
+        // //Add words to list
+        // words.Add("phone");
+        // words.Add("keyboard");
+        // words.Add("mouse");
+        // //Count words
+        // Console.WriteLine(words.Count);
+        // //List numbers
+        // List<int> numbers = new List<int>();
+        // //Refer to library at top
+        // using System.Collections.Generic;
+        // // Iterate through a list
+        // foreach (string word in words)
+        // {
+        //     Console.WriteLine(word);
+        // }
+        // //Access items by index
+        // for (int i = 0; i < words.COunt; i++)
+        // {
+        //     Console.WriteLine(words[i]);
+        // }
+        List<int> numbers = new List<int>();
+        
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {   Console.Write("Enter a list of numbers, type 0 to quit: ");
+
+            string response = Console.ReadLine();
+            userNumber = int.Parse(response);
+            
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        //Compute the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+
+        //Part 2: Average
+        float average =((float)sum) / numbers.Count;
+        Console.WriteLine($"The average of the numbers is: {average}");
+
+        //Part 3: Max
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max number is: {max}");
     }
 }
