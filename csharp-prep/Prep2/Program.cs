79c10ea33135ed384b91453fd5e61d14da592d6a
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2023-11-03 21:08:22 -0600

    Completed Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter number: ");
+        string valueFromUser = Console.ReadLine();
+
+        int x = int.Parse(valueFromUser);
+
+        if (x >= 90)
+        {
+            Console.WriteLine("You have an A");
+        }
+        else if (x >= 80 && x < 90)
+        {
+            Console.WriteLine("You have a B");
+        }
+        else if (x >= 70 && x < 80)
+        {
+            Console.WriteLine("You have a C");
+        }
+        else if (x >= 60 && x < 70)
+        {
+            Console.WriteLine("You have a D");
+        }
+        else if (x < 60)
+        {
+            Console.WriteLine("You have a F");
+        }
+        else{
+            Console.WriteLine("Your number is invalid.");
+        }
+        
     }
 }
