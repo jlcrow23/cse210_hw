386907c1ad9c105cb90c7b113f0f4f367c7b57f1
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2023-11-03 21:17:55 -0600

    Adding to Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter number: ");
+        Console.Write("What is your grade percentage? ");
         string valueFromUser = Console.ReadLine();
 
         int x = int.Parse(valueFromUser);
@@ -12,22 +12,29 @@ class Program
         if (x >= 90)
         {
             Console.WriteLine("You have an A");
+            Console.WriteLine("You are passing. Great Job!");
         }
         else if (x >= 80 && x < 90)
         {
             Console.WriteLine("You have a B");
+            Console.WriteLine("You are passing. Great Job!");
+
         }
         else if (x >= 70 && x < 80)
         {
             Console.WriteLine("You have a C");
+            Console.WriteLine("You are passing. Great Job!");
+
         }
         else if (x >= 60 && x < 70)
         {
             Console.WriteLine("You have a D");
+            Console.WriteLine("Keeping trying! You can do it!");
         }
         else if (x < 60)
         {
             Console.WriteLine("You have a F");
+            Console.WriteLine("Keep trying! You can do it!");
         }
         else{
             Console.WriteLine("Your number is invalid.");
