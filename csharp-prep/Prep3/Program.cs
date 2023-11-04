0551a48eeacb5531b4a4099d560d9b72172d35c8
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2023-11-03 21:30:44 -0600

    Step 1 Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,28 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string magic = Console.ReadLine();
+        int x = int.Parse(magic);
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int y = int.Parse(guess);
+
+        if (x > y)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (x < y)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
     }
 }
