f73de0eec27695f865d6c00995ba6cf8ca0dc2ae
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2023-11-23 11:50:41 -0700

    Learning04 program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,21 @@
+using System.Reflection.Emit;
+using System.Data;
+using Microsoft.Win32.SafeHandles;
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Julie Crowther", "Addition");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Wayne Crow", "Calculus", "5.2", "12-24");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Kenadhe Elordi", "The History of Dance", "The Tango");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }   
+    public string GetWritingInformation()
+    {
+        string studentName = GetStudentName();
+
+        return $"{_title} by {studentName}";
+    }
+}
